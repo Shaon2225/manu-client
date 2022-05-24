70cf1b8525aac0cd41f9529224d20c2a0f4a2db8
Shaon2225 <nazim1703073@stud.kuet.ac.bd>
2022-05-24 20:36:46 +0600

    user profile done

--- src/component/Dashboard/UserProfile.jsx
+++ src/component/Dashboard/UserProfile.jsx
@@ -1,31 +1,69 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import auth from "../../firebase.init";
-import {BiEdit} from 'react-icons/bi';
+import { BiEdit } from "react-icons/bi";
 import { useForm } from "react-hook-form";
+import Swal from "sweetalert2";
+import { useQuery } from "react-query";
+import Loading from "../shared/Loading";
 
 const UserProfile = () => {
   const [user] = useAuthState(auth);
-    const [editAble, setEditAble]=useState(false);
-    const {
-        register,
-        formState: { errors },
-        handleSubmit,
-      } = useForm();
+  const [editAble, setEditAble] = useState(false);
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+  const userUrl = `http://localhost:5000/userprofile/${user.email}`;
+  console.log(userUrl)
+  const {
+    data: userProfile,
+    isLoading,
+    refetch,
+  } = useQuery(["userProfile",user], () =>
+    fetch(userUrl)
+    .then((res) =>res.json())
+  );
 
-    const handleUpdate = (data) =>{
-        const profile ={
-            education: data.education,
-            social: data.social,
-            phoneNumber: data.phoneNumber,
-            address: data.address,
-            signUpDate: user.metadata.creationTime,
-        };
-        console.log(profile);
-        setEditAble(false);
-    }
+  if (isLoading) {
+    <Loading></Loading>;
+  }
 
-  console.log(user);
+  const handleUpdate = (data) => {
+    const profile = {
+      email: user.email,
+      education: data.education,
+      social: data.social,
+      phoneNumber: data.phoneNumber,
+      address: data.address,
+      signUpDate: user.metadata.creationTime,
+    };
+    fetch(`http://localhost:5000/userupdate/${user.email}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+      body: JSON.stringify(profile),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.acknowledged) {
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Your profile is updated",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+        }
+      });
+    refetch();
+    setEditAble(false);
+  };
+
+  
   return (
     <div className="mx-auto mt-5 w-3/4 flex justify-around">
       <div>
@@ -37,49 +75,85 @@ const UserProfile = () => {
           {user.email}
         </h1>
         <div>
-            <p onClick={()=>setEditAble(true)} 
-            className="text-accent text-l font-bold border-2 border-accent rounded-lg px-3 py-1 my-2 cursor-pointer w-1/2">Edit profile <button className=""><BiEdit></BiEdit></button></p>
+          <p
+            onClick={() => setEditAble(true)}
+            className="text-accent text-l font-bold border-2 border-accent rounded-lg px-3 py-1 my-2 cursor-pointer w-1/2"
+          >
+            Edit profile{" "}
+            <button className="">
+              <BiEdit></BiEdit>
+            </button>
+          </p>
         </div>
         <div className="my-5">
-            <b className="text-accent mt-10">User details:</b>
-            {
-                editAble?
-                    <form onSubmit={handleSubmit(handleUpdate)}>
-                        <div className="form-control">
-                        <label className="label text-accent font-semibold">Your University</label>
-                        <input type="text" placeholder="University/school/college"
-                        className="py-2 border border-accent rounded-lg w-full px-3 text-accent"
-                        {...register("education")
-                        }
-                        />
-                        <label className="label text-accent font-semibold">Your Socialmedia link</label>
-                        <input type="text" 
-                        placeholder="Socialmedia link"
-                        className="py-2 border border-accent rounded-lg w-full px-3 text-accent"
-                        {...register("social")
-                        }
-                        />
-                        <label className="label text-accent font-semibold">Your phone number</label>
-                        <input type="number"
-                         placeholder="phone number"
-                        className="py-2 border border-accent rounded-lg w-full px-3 text-accent"
-                        {...register("phoneNumber")
-                        }
-                        />
-                        <label className="label text-accent font-semibold">Your Address</label>
-                        <textarea type="text" placeholder="Address"
-                        className="py-2 border border-accent rounded-lg w-full px-3 text-accent"
-                        {...register("address")
-                        }
-                        />
-                        <input type="submit" value="Update" className="btn btn-nutral" />
-                        </div>
-                    </form>
-                :
-                <div>
-
-                </div>
-            }
+          <b className="text-accent mt-10">User details:</b>
+          {editAble ? (
+            <form onSubmit={handleSubmit(handleUpdate)}>
+              <div className="form-control">
+                <label className="label text-accent font-semibold">
+                  Your University
+                </label>
+                <input
+                  type="text"
+                  placeholder="University/school/college"
+                  className="py-2 border border-accent rounded-lg w-full px-3 text-accent"
+                  {...register("education")}
+                />
+                <label className="label text-accent font-semibold">
+                  Your Socialmedia link
+                </label>
+                <input
+                  type="text"
+                  placeholder="Socialmedia link"
+                  className="py-2 border border-accent rounded-lg w-full px-3 text-accent"
+                  {...register("social")}
+                />
+                <label className="label text-accent font-semibold">
+                  Your phone number
+                </label>
+                <input
+                  type="number"
+                  placeholder="phone number"
+                  className="py-2 border border-accent rounded-lg w-full px-3 text-accent"
+                  {...register("phoneNumber")}
+                />
+                <label className="label text-accent font-semibold">
+                  Your Address
+                </label>
+                <textarea
+                  type="text"
+                  placeholder="Address"
+                  className="py-2 border border-accent rounded-lg w-full px-3 text-accent"
+                  {...register("address")}
+                />
+                <input
+                  type="submit"
+                  value="Update"
+                  className="btn btn-nutral mt-3"
+                />
+                <button className="btn btn-nutral mt-3" onClick={()=>setEditAble(false)}>Cancel</button>
+              </div>
+            </form>
+          ) : (
+            <div className="mt-5 ml-5">
+              <h1 className="text-accent text-base">
+                <b>Education : </b>
+                {userProfile?.education}
+              </h1>
+              <h1 className="text-accent text-base">
+                <b>Phone no : </b>
+                {userProfile?.phoneNumber}
+              </h1>
+              <h1 className="text-accent text-base">
+                <b>Social link : </b>
+                {userProfile?.social}
+              </h1>
+              <h1 className="text-accent text-base">
+                <b>Address : </b>
+                {userProfile?.address}
+              </h1>
+            </div>
+          )}
         </div>
       </div>
 
@@ -87,7 +161,7 @@ const UserProfile = () => {
       {user?.photoURL ? (
         <div class="avatar">
           <div class="w-24 h-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-            <img src={user.photoURL}/>
+            <img src={user.photoURL} />
           </div>
         </div>
       ) : (
